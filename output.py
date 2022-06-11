9facd959c7bfc4df14a7bf57b9feee570614518a
Aditya Chhibber <102915313+Adityachhibber02@users.noreply.github.com>
2022-06-11 22:03:42 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,44 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Aditya\Desktop\Assignment 4.py ===============
+Enter marks59
+grade= C
+year2016
+2016 is a leap year
+
+How many questions needed to be there in test :
+4
+
+Q - 1
+What is 7 x 4 ?
+Enter your answer :
+28
+Your given answer is Correct
+
+
+
+Q - 2
+What is 1 x 6 ?
+Enter your answer :
+6
+Your given answer is Correct
+
+
+
+Q - 3
+What is 2 x 8 ?
+Enter your answer :
+7
+Your given answer is Wrong
+
+
+
+Q - 4
+What is 9 x 9 ?
+Enter your answer :
+85
+Your given answer is Wrong
+
+
+177 is the correct answer
